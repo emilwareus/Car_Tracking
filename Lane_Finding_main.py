fb8654f523c7238247b65b8f9b4dcfc3cd929472
emilwareus <emil.wareus47@gmail.com>
2017-11-12 19:25:23 +0100

    agaaga

--- Lane_Finding_main.py
+++ Lane_Finding_main.py
@@ -24,17 +24,7 @@ Lanes = Lanes()
 def process_img(image):
     #Due to that openCV is used to read the image
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #Undistort image
-
-    #Run the threashold algorithms, this Consists of: 
-    #dir_binary = Thresh.dir_threashold(image, sobel_kernel=sobel_kernel, thresh=dir_thresh)
-    #gradx = Thresh.abs_sobel_thresh(image, sobel_kernel=sobel_kernel, orient = 'x', thresh = x_thresh)
-    #grady = Thresh.abs_sobel_thresh(image, sobel_kernel=sobel_kernel, orient = 'y', thresh = y_thresh)
-    #mag = Thresh.mag_threashold(image, sobel_kernel=sobel_kernel, thresh=mag_thresh)
-    #color = Thresh.color_thresh(image, thresh_s =(170, 255), thresh_h = (15, 100))
-    #combined = np.zeros_like(dir_binary)
-    #combined[(((gradx == 1) & (grady == 1) & (color == 1)) | (((mag == 1)| (color == 1)) & (dir_binary == 1)) )] = 1
-                 
+    
     combined = Thresh.threshold(image)
 
     #Perspective change
@@ -71,32 +61,3 @@ def get_lane_image(image):
     #Remove this when makeing movie
     #img_with_lines = cv2.cvtColor(img_with_lines, cv2.COLOR_RGB2BGR)
     return img_with_lines
-
-
-''' 
-mtx, dist = Distort.calibrate()
-for i in range(1,6):
-   
-    image = cv2.imread('test_images/test{}.jpg'.format(i)) 
-    
-    #Pipeline
-    Lanes.init_lanes()
-    img_with_lines, left_fitx, right_fitx, ploty, exist, combined, per_img_C = process_img(image)
-    #Thresholds of image
-    
-    plt.figure(figsize= (24, 9))
-    plt.imshow(img_with_lines) #, cmap='gray'
-
-#Lets make some Movies: 
-MakeMovie = False
-if MakeMovie ==True:
-    from moviepy.editor import VideoFileClip
-
-    
-    vid_output = 'output_1.mp4'
-    clip1 = VideoFileClip("project_video.mp4")
-    vid_clip = clip1.fl_image(get_lane_image)
-    vid_clip.write_videofile(vid_output, audio=False)
-    
-    
-'''
